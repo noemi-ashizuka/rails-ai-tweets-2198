4eab66f8487bfdca99a792309ca5235f563160be
noemi ashizuka <noemi.calanducci@gmail.com>
2026-03-05 10:25:14 +0900

    added comments

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -11,6 +11,9 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new(tweet_params)
     @tweet.shortened = RubyLLM.chat.with_temperature(0.8).ask("Generate an unhinged tweet from this text: #{@tweet.long}").content
     if @tweet.save
+      # run job from controller:
+      # GenerateImageJob.perform_later(@tweet) unless @tweet.photo.attached?
+      # not needed if you have a callback in Tweet model
       redirect_to tweet_path(@tweet), notice: "Tweet successfully created"
     else
       render :new, status: :unprocessable_entity
